0cd3482ee4cc516290f9cfc3782832b71864cf4e
hokto <hokuto.k1441@gmail.com>
2019-07-14 10:12:33 +0900

    実際に通ったルートも表示できるようにした

--- source/Routes.py
+++ source/Routes.py
@@ -15,7 +15,7 @@ def Search_route(APIKEY,start,goal):#最適ルートを取得する関数。
         place=result["Feature"][i]["Geometry"]["Coordinates"]
         list_places.append(place)
     safty_places=Search_safty(list_places)#取得した場所の中から安全な場所を取得
-    Making_route(APIKEY,str(start.lat)+","+str(start.lon),safty_places,str(goal.lat)+","+str(goal.lon))
+    Making_route(APIKEY,str(start.lat)+","+str(start.lon),safty_places,str(goal.lat)+","+str(goal.lon),{"31.760254,131.080396","31.7619512,131.0828761"})
 
 def Search_safty(list_places):#安全な場所を探索する関数。
     list_ARV=[]#ARV（最大増振率）を格納
@@ -63,9 +63,9 @@ def Sort_places(list_places,list_ARV,left,right):#ARVが小さい順に場所と
         Sort_places(list_places,list_ARV,left,k-1)
     if(k+1<right):
         Sort_places(list_places,list_ARV,k+1,right)
-def Making_route(APIKEY,start,list_via,goal):#最適ルート作成関数。(おそらく実際に通ったルートも同時に表示)
-    url="https://map.yahooapis.jp/course/V1/routeMap?appid={apikey}&route={start_place},{via_places},{goal_place}"
-    access_url=url.format(apikey=APIKEY,start_place=start,via_places=",".join(list_via),goal_place=goal)
+def Making_route(APIKEY,start,list_via,goal,list_realRoute):#最適ルートと、実際に通ったルートの作成関数。
+    url="https://map.yahooapis.jp/course/V1/routeMap?appid={apikey}&route={RealRoute}&route={start_place},{via_places},{goal_place}|color:ff000099"
+    access_url=url.format(apikey=APIKEY,start_place=start,via_places=",".join(list_via),goal_place=goal,RealRoute=",".join(list_realRoute))
     Download_route(access_url,"../img/route.png")
 def Download_route(url,file_path):#探索したルートの画像を取得して保存する関数。
     try:
@@ -77,7 +77,7 @@ def Download_route(url,file_path):#探索したルートの画像を取得して
         print(e)
 start_place=Pos()
 goal_place=Pos()
-start_place.lat=31.760254
+start_place.lat=31.760254#仮設定　スタート：都城高専　ゴール：沖水小
 start_place.lon=131.080396
 goal_place.lat=31.7643
 goal_place.lon=131.088242
