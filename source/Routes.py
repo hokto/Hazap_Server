547720fb2d9fbbe68fd5f7b7abdb6e24912b9884
hokto <hokuto.k1441@gmail.com>
2019-07-17 16:05:35 +0900

    避難経路探索方法を調整

--- source/Routes.py
+++ source/Routes.py
@@ -1,12 +1,14 @@
 import requests
 import json
 import urllib
+
 class Pos:#緯度、経度格納クラス。
     lat=0.0
     lon=0.0
+
 APIKEY="dj00aiZpPWNIMG5nZEpkSXk3OSZzPWNvbnN1bWVyc2VjcmV0Jng9ZDk-"
 def Search_route(APIKEY,start,goal):#最適ルートを取得する関数。
-    url="https://map.yahooapis.jp/spatial/V1/shapeSearch?mode=circle&appid={key}&coordinates={start_lon},{start_lat} {goal_lon},{goal_lat}&sort=box&results=100&output=json"
+    url="https://map.yahooapis.jp/spatial/V1/shapeSearch?mode=circle&appid={key}&coordinates={start_lon},{start_lat} {start_lon},{start_lat} {goal_lon},{goal_lat} {goal_lon},{goal_lat}&sort=box&results=100&output=json"
     access_url=url.format(key=APIKEY,start_lat=start.lat,start_lon=start.lon,goal_lat=goal.lat,goal_lon=goal.lon)#必要なデータの代入
     result=requests.get(access_url)#データをjsonで取得し、連想配列に変換
     result=result.json()
@@ -16,6 +18,7 @@ def Search_route(APIKEY,start,goal):#最適ルートを取得する関数。
         list_places.append(place)
     safty_places=Search_safty(list_places)#取得した場所の中から安全な場所を取得
     Making_route(APIKEY,str(start.lat)+","+str(start.lon),safty_places,str(goal.lat)+","+str(goal.lon),{"31.760254,131.080396","31.7619512,131.0828761"})
+
 
 def Search_safty(list_places):#安全な場所を探索する関数。
     list_ARV=[]#ARV（最大増振率）を格納
@@ -35,6 +38,8 @@ def Search_safty(list_places):#安全な場所を探索する関数。
             break
         safty_places.append(list_places[i])
     return safty_places
+
+
 def Sort_places(list_places,list_ARV,left,right):#ARVが小さい順に場所とARVをソート（クイックソート）
     i=left+1
     k=right
@@ -63,10 +68,14 @@ def Sort_places(list_places,list_ARV,left,right):#ARVが小さい順に場所と
         Sort_places(list_places,list_ARV,left,k-1)
     if(k+1<right):
         Sort_places(list_places,list_ARV,k+1,right)
+
+
 def Making_route(APIKEY,start,list_via,goal,list_realRoute):#最適ルートと、実際に通ったルートの作成関数。
     url="https://map.yahooapis.jp/course/V1/routeMap?appid={apikey}&route={RealRoute}&route={start_place},{via_places},{goal_place}|color:ff000099"
     access_url=url.format(apikey=APIKEY,start_place=start,via_places=",".join(list_via),goal_place=goal,RealRoute=",".join(list_realRoute))
     Download_route(access_url,"../img/route.png")
+
+
 def Download_route(url,file_path):#探索したルートの画像を取得して保存する関数。
     try:
         with urllib.request.urlopen(url) as web_file:
@@ -75,6 +84,8 @@ def Download_route(url,file_path):#探索したルートの画像を取得して
                 local_file.write(data)
     except urllib.error.URLError as e:
         print(e)
+
+
 start_place=Pos()
 goal_place=Pos()
 start_place.lat=31.760254#仮設定　スタート：都城高専　ゴール：沖水小
