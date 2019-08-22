c0d03d95257fc30e38abb6612260f1d9403a96c2
hagihara1102 <hagitaku1102@gmail.com>
2019-08-22 10:04:52 +0900

    サーバーのモードを更新しました

--- source/server.py
+++ source/server.py
@@ -9,45 +9,53 @@ def server():
         # IPアドレスとポートを指定
         n=0
         Coordinates={}
-        CoordinateLogs=[]
-        s.bind(('192.168.11.2', 4000))
+        CoordinateLogs={}
+        s.bind(('192.168.11.133', 4000))
         # 1 接続
         s.listen(1)
         # connection するまで待つ
         while True:
+            send="Invalid"
             # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
             conn, addr = s.accept()
             with conn:
                 while True:
                     # データを受け取る
-                    data = conn.recv(2048)
+                    data = conn.recv(4096)
                     if not data:
                         break
                     rec=data.decode()
                     splited=rec.split(":")
                     if(splited[0]=="Recruit" and startflg==0):
-                        send="your number is"+str(n)
-                        CoordinateLogs.append([])
-                        CoordinateLogs[n].append(splited[1].split(","))
+                        send=str(n)
+                        CoordinateLogs[n]=[]
+                        pl=splited[1].split(",")
+                        CoordinateLogs[n].append(pl)
                         Coordinates[n]=splited[1].split(",")
                         n+=1
+                        print(CoordinateLogs)
                     elif splited[0]=="Recruit" and startflg==1:
                         send="started"
+                    elif splited[0]=="Cancel":
+                        CoordinateLogs.pop(int(splited[1]))
                     elif splited[0]=="Start":
                         startflg = 1
                         send="start!"
                     elif splited[0]=="Number" and startflg==1:
-                        Coordinates[int(splited[1])]=splited[2].split(",")
-                        CoordinateLogs[int(splited[1])].append(splited[2].split(","))
-                        send=str(count[int(splited[1])]/len(count))
+                        if int(splited[1]) in CoordinateLogs:
+                            Coordinates[int(splited[1])]=splited[2].split(",")
+                            CoordinateLogs[int(splited[1])].append(splited[2].split(","))
+                            send="Around:"+str(count[int(splited[1])])+"N="+str(n)
+                        else:
+                            send="Failed"
                     elif splited[0]=="End":
                         send="OK"
                         conn.sendall(send.encode())
                         return 0
                     if(startflg==1):
-                        print("CoordinateLogs",CoordinateLogs[1])
                         count=getplace.Calcudens(Coordinates)
                     conn.sendall(send.encode())
+                    print(CoordinateLogs)
                     
 
 if __name__=="__main__":
