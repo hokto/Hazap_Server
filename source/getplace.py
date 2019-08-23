10ccfea50888c4209106c8a70b67ae6675e3ff43
hagihara1102 <hagitaku1102@gmail.com>
2019-08-23 13:20:47 +0900

    関数のリファクタリングをしました

--- source/getplace.py
+++ source/getplace.py
@@ -4,14 +4,16 @@ import urllib.request
 import HazapModules
 import math
 import Routes
+import time
 
 def get_Coordinates(pos):
-    #この関数は緯度,経度を投げればいい感じのを返してくれる
+    #この関数は緯度,経度を投げればその地点からの避難場所を返してくれる
     sta = {
         "appid": "dj00aiZpPWlGdHd2QlFKTDZZWiZzPWNvbnN1bWVyc2VjcmV0Jng9ODg-", 
         "output":"&output=json"
         }
     redata=[]
+    tellist=[]
     url="https://map.yahooapis.jp/search/local/V1/localSearch?appid="+sta["appid"]+"&lat="+str(pos.lat)+"&lon="+str(pos.lon)+"&dist=2"+sta["output"]+"&gc=0425&sort=geo"
     url2="https://map.yahooapis.jp/search/local/V1/localSearch?appid="+sta["appid"]+"&lat="+str(pos.lat)+"&lon="+str(pos.lon)+"&dist=2"+sta["output"]+"&gc=0423007&sort=geo"
     url3="https://map.yahooapis.jp/search/local/V1/localSearch?appid="+sta["appid"]+"&lat="+str(pos.lat)+"&lon="+str(pos.lon)+"&dist=2"+sta["output"]+"&gc=0305007&sort=geo"
@@ -24,20 +26,24 @@ def get_Coordinates(pos):
     #この下の処理で座標をリストにまとめてreturn
     if(data["ResultInfo"]["Count"]!=0):
         for i in data["Feature"]:
-            redata.append(i["Geometry"]["Coordinates"].split(','))
+            if(tellist.count(i["Property"]["Tel1"])==0):
+                tellist.append(i["Property"]["Tel1"])
+                redata.append(i["Geometry"]["Coordinates"].split(','))
     if(data2["ResultInfo"]["Count"]!=0):
         for i in data2["Feature"]:
-            redata.append(i["Geometry"]["Coordinates"].split(','))
+            if(tellist.count(i["Property"]["Tel1"])==0):
+                tellist.append(i["Property"]["Tel1"])
+                redata.append(i["Geometry"]["Coordinates"].split(','))
     if(data3["ResultInfo"]["Count"]!=0):
         for i in data3["Feature"]:
-            redata.append(i["Geometry"]["Coordinates"].split(','))
-
-
+            if(tellist.count(i["Property"]["Tel1"])==0):
+                tellist.append(i["Property"]["Tel1"])
+                redata.append(i["Geometry"]["Coordinates"].split(','))
     for i in range(len(redata)):
         redata[i][0],redata[i][1]=redata[i][1],redata[i][0]
     return redata
 
-def Reray(pos1,pos2):
+def Reray(pos1,pos2,name):
     sta = {
         "appid": "dj00aiZpPWlGdHd2QlFKTDZZWiZzPWNvbnN1bWVyc2VjcmV0Jng9ODg-", 
         "output":"&output=json"
@@ -46,31 +52,41 @@ def Reray(pos1,pos2):
     url="https://map.yahooapis.jp/spatial/V1/shapeSearch?appid="+sta["appid"]+"&coordinates="+str(pos1.lon)+","+str(pos1.lat)+"%20"+str(pos1.lon)+","+str(pos1.lat)+"%20"+str(pos2.lon)+","+str(pos2.lat)+"&mode=line"+sta["output"]
     res=urllib.request.urlopen(url)
     data=json.loads(res.read().decode())
-    #print(data)
+
     for i in data["Feature"]:
         foo=i["Geometry"]["Coordinates"].split(",")
-        foo[0],foo[1]=foo[1],foo[0]
-        redata[i["Name"]]=foo
-        suburl="https://map.yahooapis.jp/inner/V1/building?lat="+foo[0]+"&lon="+foo[1]+"&appid="+sta["appid"]+sta["output"]
-        subres=urllib.request.urlopen(suburl)
-        subdata=json.loads(subres.read().decode())
-        if(subdata["ResultInfo"]["Count"]!=0):
-            redata[i["Name"]].append(int(subdata["Dictionary"]["Building"][0][0]["Floor"]))
+        if i["Geometry"]["Coordinates"] not in name:
+            foo[0],foo[1]=foo[1],foo[0]
+            redata[i["Name"]]=foo
+            suburl="https://map.yahooapis.jp/inner/V1/building?lat="+foo[0]+"&lon="+foo[1]+"&appid="+sta["appid"]+sta["output"]
+            subres=urllib.request.urlopen(suburl)
+            subdata=json.loads(subres.read().decode())
+           ## print(subdata,"\n\n")
+            if(subdata["ResultInfo"]["Count"]!=0):
+                name[i["Geometry"]["Coordinates"]]=int(subdata["Dictionary"]["Building"][0][0]["Floor"])
+                redata[i["Name"]].append(int(subdata["Dictionary"]["Building"][0][0]["Floor"]))
+            else:
+                name[i["Geometry"]["Coordinates"]]=1
+                redata[i["Name"]].append(1)
         else:
-            redata[i["Name"]].append(1)
+            redata[i["Name"]]=foo
+            redata[i["Name"]].append(name[i["Geometry"]["Coordinates"]])
     return redata
 
 def searchplace(pos):
+    #この関数は座標を投げるとその地点からいい避難場所への道のりにある建物の階数、つまり高さを足していって低い順にソートして返す
     data=get_Coordinates(pos)
     hoge={}
+    name={}
     sumstep=[]
     for i in range(len(data)):
         sumstep.append(0)
+
     for i in range(len(data)):
         pos2=HazapModules.Coordinates()
         pos2.lat=data[i][0]
         pos2.lon=data[i][1]
-        hoge[i]=Reray(pos,pos2)
+        hoge[i]=Reray(pos,pos2,name)
         for k in hoge[i]:
             sumstep[i]+=hoge[i][k][2]
         hoge[i]["step"]=sumstep[i]
@@ -134,10 +150,11 @@ def GenerateHazard(Coordinates):
     for i in range(n):
         mark+="&pin"+str(i+1)+"="+hoge[i]["coordinates"][0]+","+hoge[i]["coordinates"][1]
     print(mark)
+
     for i in range(len(data["features"][0]["geometry"]["coordinates"][0])):
         img+=","+str(data["features"][0]["geometry"]["coordinates"][0][i][1])+","+str(data["features"][0]["geometry"]["coordinates"][0][i][0])
     print(img)
-    yhurl="https://map.yahooapis.jp/map/V1/static?appid=dj00aiZpPWNIMG5nZEpkSXk3OSZzPWNvbnN1bWVyc2VjcmV0Jng9ZDk-&"+mark+"p=0,0,255,0,3,0,0,255,60"+img+"&z=15&width=1500&height=1500&output=png32&autoscale=on"
+    yhurl="https://map.yahooapis.jp/map/V1/static?appid=dj00aiZpPWNIMG5nZEpkSXk3OSZzPWNvbnN1bWVyc2VjcmV0Jng9ZDk-&"+mark+"&p=0,0,255,0,3,0,0,255,60"+img+"&e=0,255,0,0,5,0,255,0,127,"+str(Coordinates.lat)+","+str(Coordinates.lon)+",2000&z=16&width=1000&height=1000&output=png32"
     Routes.Download_route(yhurl,"../img/test.png")
 
     return 0
